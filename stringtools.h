c3e76a5cc64ebb3fe27489b866a6f5aa9b8b0044
Andreas Fink <andreas.fink@cscs.ch>
2018-11-23 11:29:08 +0100

    *add* added replace method to estring *change* return reference to internal std::string

--- stringtools.h
+++ stringtools.h
@@ -4,7 +4,7 @@
 #include <string>
 #include <vector>
 
-namespace {
+namespace af {
 namespace s {
 template <typename T>
 T to(const std::string& s) {
@@ -15,8 +15,8 @@ T to(const std::string& s) {
 }
 
 
-std::string str(const std::string& s) { return s; }
-std::string str(const char* s) { return s; }
+inline std::string str(const std::string& s) { return s; }
+inline std::string str(const char* s) { return s; }
 
 template <typename T>
 std::string str(const T& in) {
@@ -32,6 +32,7 @@ class estring {
   public:
     estring(const char* sz) { if (sz) m_s=sz; }
     estring(const std::string& s="") : m_s(s)  {}
+    estring(std::string&& s) : m_s(s) {}
     bool startsWith(const estring& other) const {
       return (m_s.substr(0, other.m_s.length()) == other.m_s);
     }
@@ -52,25 +53,43 @@ class estring {
       } while(pos != std::string::npos);
       return ret;
     }
+    estring& replace(const estring& searchString, const estring& replaceString) {
+      size_t pos=0;
+      while (pos != std::string::npos) {
+        pos = m_s.find(searchString, pos);
+        if (pos != std::string::npos) {
+          m_s.replace(pos, searchString.size(), replaceString);
+        }
+      }
+      return *this;
+    }
     template<typename T> T to() const {
       std::istringstream is(m_s);
       T ret = T();
       is >> ret;
       return ret;
     }
+    size_t size() const { return m_s.size(); }
     bool empty() const { return m_s.empty(); }
     bool operator==(const estring& o) const { return o.m_s == m_s; }
     bool operator!=(const estring& o) const { return o.m_s != m_s; }
-    operator std::string() const { return m_s; }
+    estring& operator+(const char* o) { m_s+=o; return *this; }
+    operator const std::string&() const { return m_s; }
+    operator std::string&() { return m_s; }
     const char* c_str() const { return m_s.c_str(); }
     const std::string& str() const { return m_s; }
 };
 } // s namespace
-} // anonymous namespace
+inline s::estring& operator+(const char* sz, s::estring& es) {
+  std::string& internalString = es;
+  internalString = sz + internalString;
+  return es;
+}
+} // af namespace
 
 namespace std {
-ostream& operator<<(ostream& dest, const ::s::estring& s) {
-  dest << s.c_str();
+inline ostream& operator<<(ostream& dest, const af::s::estring& s) {
+  return (dest << s.c_str());
 }
 }
 
